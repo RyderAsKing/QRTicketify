a759861063f138da9363a65614d6fd454ba576e9
RyderAsking <clasherlodt@gmail.com>
2022-11-15 16:53:44 +0530

    ticket creation form

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -33,6 +33,7 @@ class TicketController extends Controller
     public function create()
     {
         //
+        return view('tickets.create');
     }
 
     /**
---
+++ resources/views/tickets/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 mt-2">
+            <div class="card">
+                <div class="card-header">Create a new ticket</div>
+                <div class="card-body">
+                    <form method="post" action="{{route('tickets.store')}}">
+                        @csrf
+                        <div class="form-group mb-2">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                                placeholder="Enter email">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Create a ticket</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

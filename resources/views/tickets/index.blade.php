6bb44617a27bc05d1aa6662643265710ebc41763
RyderAsking <clasherlodt@gmail.com>
2022-11-15 15:46:03 +0530

    migration for tickets

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -6,14 +6,44 @@
         <div class="col-md-12 mt-2">
             <div class="card">
                 <div class="card-header">Tickets</div>
+
+                <div class="card-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">ID</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    {{ $tickets->links() }}
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="buttons">
-        <a href="#" class="btn btn-success mt-2 text-white">Create a new ticket</a> <a href="#"
-            class="btn btn-warning mt-2">Manage
-            tickets</a>
-    </div>
 </div>
 @endsection

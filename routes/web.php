a52ed6f68d43b589da702ebc72159e669fcf5bd1
RyderAsking <clasherlodt@gmail.com>
2022-11-17 20:41:51 +0530

    made home route /

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes(['register' => false]);
 
-Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name(
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name(
     'home'
 );
 

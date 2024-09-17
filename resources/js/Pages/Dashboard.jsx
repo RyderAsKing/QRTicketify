00ee1605258e2a0fe2ebf53ffc4bd5e13c290eeb
Rajat Asthana <clasherlodt@gmail.com>
2024-09-18 00:07:52 +0530

    listing all events on the dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -59,14 +59,9 @@ export default function Dashboard() {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div>
+                    <Events events={events} />
                 </div>
             </div>
-            <Events events={events} />
         </AuthenticatedLayout>
     );
 }

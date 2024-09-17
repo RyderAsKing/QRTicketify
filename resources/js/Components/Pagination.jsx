99220675c9bb0607a33ca3a53aec056c0b53cccb
Rajat Asthana <clasherlodt@gmail.com>
2024-09-18 01:31:30 +0530

    updated pagination colour

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -56,7 +56,7 @@ export default function Pagination({ paginationData }) {
                                 href={link.url || "#"}
                                 className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
                                     link.active
-                                        ? "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        ? "z-10 bg-gray-800 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                         : "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                                 }`}
                                 dangerouslySetInnerHTML={{ __html: link.label }}
